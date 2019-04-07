07f2a93c8340c4ac8d4607124ea95aa6dda9eeb5
tymoteuszilczyszyn <tymoteuszilczyszyn@tymoteuszs-macbook-pro.home>
2019-04-07 19:31:15 +0200

    Tests: CharacterTest class modified

--- Cluedo/test/cluedo/gameLogic/CharacterTest.java
+++ Cluedo/test/cluedo/gameLogic/CharacterTest.java
@@ -6,16 +6,13 @@
 package cluedo.gameLogic;
 
 import cluedo.gameLogic.Character.CharacterType;
-import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 /**
  *
- * @author tymoteuszilczyszyn
+ * @author Tymek
  */
 public class CharacterTest {
     
