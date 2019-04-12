2ccedf021abca2eb5acb0c535b3ce268dc64e29f
matthewwhittenham <mw439@sussex.ac.uk>
2019-04-12 15:16:26 +0100

    Update Player.java

--- Cluedo/src/cluedo/gameLogic/Player.java
+++ Cluedo/src/cluedo/gameLogic/Player.java
@@ -1,14 +1,52 @@
+
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 package cluedo.gameLogic;
-
+import java.util.ArrayList;
 /**
  *
  * @author Jamie
  */
-public class Player {
+public class Player
+{
+    private ArrayList<ClueCard> card;
+    private Character character;
+    private DetectiveTable detectiveTable;  // To be implemented later
+
+    public Player(Character character)
+    {
+        card = new ArrayList<>();
+        this.character = character;
+    }
+    
+    public void Move()
+    {
+        
+    }
+    
+    public void makeSuggestion(Character character, Room room, Weapon weapon)
+    {
+        
+    }
+    
+    public boolean makeAccusation(Character character, Room room, Weapon weapon)
+    {
+        
+    }
+    
+    public ClueCard respondToSuggestion(Suggestion suggestion)
+    {
+        
+    }
+    
+    public void markDetectiveTable(ClueType cluetype, Player clueGiver)
+    {
+        
+    }
+    
     
 }
