1fe51402cab4aec131aa05ee54814bd5d50658b5
tymoteuszilczyszyn <tymoteuszilczyszyn@tymoteuszs-macbook-pro.home>
2019-04-17 11:53:02 +0200

    classes created: DetNoteType, DetectiveNotes

---
+++ Cluedo/src/cluedo/gameLogic/DetectiveNotes.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cluedo.gameLogic;
+
+import java.util.HashMap;
+
+/**
+ *
+ * @author tymoteuszilczyszyn
+ */
+public class DetectiveNotes {
+    private HashMap<Player, HashMap<ClueType, DetNoteType>> detectiveTable ;
+    private String detectiveNotes;
+    
+    public void markTable(Player p1, DetNoteType dnt1) {
+        
+    }
+    
+    public void writeNotes(String n1) {
+        
+    }
+
+    
+}
